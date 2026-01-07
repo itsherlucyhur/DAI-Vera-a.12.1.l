44138aebd758d8d982dc5d3224604647c0b580ad
Lucy Hur <lucyhur2426@gmail.com>
2026-01-07 11:01:07 -0500

    commit

--- DAI-Vera-main-2/convertImagesToJpeg.m
+++ DAI-Vera-main-2/convertImagesToJpeg.m
@@ -1,19 +1,30 @@
-function [newDirectory] = convertImagesToJpeg(directory, threeDVolume, labelName)
+function [newDirectory] = convertImagesToJpeg(baseDir, dicomFolder, labelName)
     
     logDAI('Inside convertImagesToJpeg()');
     logDAI('Calling getJPEGVolumeFromDicomVolume with number of channels = 1');
-    jpegVolume = getJPEGVolumeFromDicomVolume(threeDVolume, 1);
+
+    % Read DICOM files from folder safely
+    jpegVolume = getJPEGVolumeFromDicomVolume(dicomFolder, 1);
     logDAI('JPEG volume successfully created.');
-    logDAI('JEPGData Directory is located at:');
-    newDirectory = strcat(directory, labelName, '\');
+
+    % Create output directory (macOS + Windows safe)
+    newDirectory = fullfile(baseDir, labelName);
+    logDAI('JPEGData Directory is located at:');
     logDAI(newDirectory);
-    logDAI('Creating JPEGData directory');
-    mkdir (newDirectory);
-    logDAI('Creating JPEGData directory is successful');
-    logDAI('Writing JPEG Images');
+
+    if ~exist(newDirectory, 'dir')
+        mkdir(newDirectory);
+        logDAI('JPEGData directory created successfully.');
+    else
+        logDAI('JPEGData directory already exists.');
+    end
+
+    % Write JPEG images
+    logDAI('Writing JPEG Images...');
     n = size(jpegVolume, 3);
     for i = 1:n
-        labelFileName = strcat(newDirectory, num2str(i), '.jpg');
+        labelFileName = fullfile(newDirectory, sprintf('%03d.jpg', i));
         imwrite(jpegVolume(:,:,i), labelFileName);
     end
+    logDAI('All JPEG Images written successfully.');
 end
